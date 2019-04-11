66a421cfe6a26e29325139b76daa1ae516fd407e
Patrick Faion <p.faion@web.de>
2019-04-11 23:32:28 +0200

    Wrap code in class, use numpy and better projections

--- beautymaps.py
+++ beautymaps.py
@@ -1,97 +1,105 @@
-# %% mercator projection helper functions
 import math
 
-def merc_x(lon):
-    r_major=6378137.000
-    return r_major*math.radians(lon)
-
-def merc_y(lat):
-    if lat>89.5:lat=89.5
-    if lat<-89.5:lat=-89.5
-    r_major=6378137.000
-    r_minor=6356752.3142
-    temp=r_minor/r_major
-    eccent=math.sqrt(1-temp**2)
-    phi=math.radians(lat)
-    sinphi=math.sin(phi)
-    con=eccent*sinphi
-    com=eccent/2
-    con=((1.0-con)/(1.0+con))**com
-    ts=math.tan((math.pi/2-phi)/2)/con
-    y=0-r_major*math.log(ts)
-    return y
-
-def project(lat, lon, **kwargs):
-    return merc_x(lon), merc_y(lat)
-
- # %% overpy test
-import overpy
-
-api = overpy.Overpass()
-
-# fetch all ways and nodes
-result = api.query(
-"""
-(
-way
-  ["highway"~"^motorway|trunk|primary|secondary|tertiary|residential|living_street$"]
-  (57.515306, 25.365349, 57.561689, 25.459442);
-  >;
-);
-out;
-""")
-
-# for way in result.ways:
-#     print("Name: %s" % way.tags.get("name", "n/a"))
-#     print("  Highway: %s" % way.tags.get("highway", "n/a"))
-#     print("  Nodes:")
-#     for node in way.nodes:
-#         print("    Lat: %f, Lon: %f" % (node.lat, node.lon))
-
-
-# %% convert ways to x/y
-inf = float("inf")
-xrange = (inf, -inf)
-yrange = (inf, -inf)
-pixelways = []
-for way in result.ways:
-    tmp = []
-    for node in way.nodes:
-        x, y = project(**node.__dict__)
-        xrange = (min(xrange[0], x), max(xrange[1], x))
-        yrange = (min(yrange[0], y), max(yrange[1], y))
-        tmp.append((x, y))
-    pixelways.append(tmp)
-
-scale = 10
-
-for i, way in enumerate(pixelways):
-    for j, (x, y) in enumerate(way):
-        pixelways[i][j] = ((x - xrange[0])/scale, (y - yrange[0])/scale)
-
-width = math.ceil(xrange[1]/scale - xrange[0]/scale)
-height = math.ceil(yrange[1]/scale - yrange[0]/scale)
-
-# %% test print w cairo
 import cairo
+import numpy as np
+import overpy
+from pyproj import Proj
 
 
+class Beautymap:
 
-margin = 50
+    @staticmethod
+    def bbox_from_centered(center_latlon, size):
+        # quick and dirty conversion of cathographic to geodetic distances
+        # see: https://gis.stackexchange.com/a/2964
+        # TODO: use pyproj for this as well!
+        lat, lon = center_latlon
+        delta_lat = size / 111.111
+        delta_lon = size / (111.111 * math.cos(lat))
+        bbox = (lat - delta_lat, lon - delta_lon, lat + delta_lat, lon + delta_lon)
+        return bbox
 
-with cairo.ImageSurface(cairo.FORMAT_ARGB32, width + 2*margin, height + 2*margin) as surface:
-    context = cairo.Context(surface)
-    context.scale(1, 1)
-    context.rectangle(0, 0, width + 2*margin, height + 2*margin)
-    context.set_source_rgb(1, 1, 1)
-    context.fill()
-    context.set_source_rgb(0, 0, 0)
-    context.set_line_width(1)
-    for way in pixelways:
-        x, y = way[0]
-        context.move_to(int(x) + margin, int(y) + margin)
-        for x, y in way:
-            context.line_to(int(x) + margin, int(y) + margin)
-    context.stroke()
 
-    surface.write_to_png("example.png")
+    @classmethod
+    def centered(cls, center_latlon, size):
+        bbox = cls.bbox_from_centered(center_latlon, size)
+        return cls(bbox)
+
+
+    def __init__(self, bbox):
+        self.bbox = bbox
+        self.road_types = {
+            'motorway',
+            'trunk',
+            'primary',
+            'secondary',
+            'tertiary',
+            'residential',
+            'living_street',
+        }
+
+        self.raw_overpass_data = self.get_overpass_data()
+
+        self.geodetic_data = [
+            np.array([(node.lat, node.lon) for node in way.nodes], dtype=float)
+            for way in self.raw_overpass_data
+        ]
+
+        # EPSG.3857 projection https://epsg.io/3857
+        # Pseudo-Mercator as used by Google Maps and Open Street Maps
+        proj = Proj(3857)
+        self.cathographic_data = [
+            # projector works with separate arrays of longs and lats (!)
+            np.vstack(proj(way[:, 1], way[:, 0])).T
+            for way in self.geodetic_data
+        ]
+
+        self.minimum = np.min([way.min(axis=0) for way in self.cathographic_data], axis=0)
+        self.maximum = np.max([way.max(axis=0) for way in self.cathographic_data], axis=0)
+        self.range = self.maximum - self.minimum
+
+        self.normalized_data = [
+            (way - self.minimum)
+            for way in self.cathographic_data
+        ]
+
+
+    def get_overpass_data(self):
+        overpass_ql_query = f"""
+            (
+            way
+                // filter road types with OR regex
+                ["highway"~"^{'|'.join(self.road_types)}$"]
+                {str(self.bbox)};
+                >;
+            );
+            out;
+        """
+        return overpy.Overpass().query(overpass_ql_query).ways
+
+
+    def render_png(self, filename):
+        width, height = np.rint(self.range).astype(int)
+
+        with cairo.ImageSurface(cairo.FORMAT_ARGB32, width, height) as surface:
+            ctx = cairo.Context(surface)
+            ctx.scale(1, 1)
+            ctx.rectangle(0, 0, width, height)
+            ctx.set_source_rgb(1, 1, 1)
+            ctx.fill()
+            ctx.set_source_rgb(0, 0, 0)
+            ctx.set_line_width(1)
+            for way in self.normalized_data:
+                int_way = np.rint(way).astype(int)
+                x, y = int_way[0, :]
+                ctx.move_to(x, height - y)
+                for x, y in int_way[1:]:
+                    ctx.line_to(x, height - y)
+            ctx.stroke()
+
+            surface.write_to_png(filename)
+
+
+if __name__ == '__main__':
+    m = Beautymap.centered(center_latlon=(57.538498, 25.412396), size=1)
+    m.render_png("test.png")

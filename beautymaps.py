bfdaf8cb24d0586ee4a9a612f2c9898a08174831
Patrick Faion <p.faion@web.de>
2019-04-13 10:08:54 +0200

    Improve rendering by centering, scaling and adding padding

--- beautymaps.py
+++ beautymaps.py
@@ -40,6 +40,11 @@ class Beautymap:
 
         self.raw_overpass_data = self.get_overpass_data()
 
+        self.road_data = [
+            way.tags.get('highway', '')
+            for way in self.raw_overpass_data
+        ]
+
         self.geodetic_data = [
             np.array([(node.lat, node.lon) for node in way.nodes], dtype=float)
             for way in self.raw_overpass_data
@@ -52,15 +57,6 @@ class Beautymap:
             # projector works with separate arrays of longs and lats (!)
             np.vstack(proj(way[:, 1], way[:, 0])).T
             for way in self.geodetic_data
-        ]
-
-        self.minimum = np.min([way.min(axis=0) for way in self.cathographic_data], axis=0)
-        self.maximum = np.max([way.max(axis=0) for way in self.cathographic_data], axis=0)
-        self.range = self.maximum - self.minimum
-
-        self.normalized_data = [
-            (way - self.minimum)
-            for way in self.cathographic_data
         ]
 
 
@@ -78,28 +74,54 @@ class Beautymap:
         return overpy.Overpass().query(overpass_ql_query).ways
 
 
-    def render_png(self, filename):
-        width, height = np.rint(self.range).astype(int)
+    def render_square_png(self, filename, size, padding, line_widths=dict()):
+        # 2D float
+        coord_min = np.min([way.min(axis=0) for way in self.cathographic_data], axis=0)
+        coord_max = np.max([way.max(axis=0) for way in self.cathographic_data], axis=0)
+        coord_range = coord_max - coord_min
 
-        with cairo.ImageSurface(cairo.FORMAT_ARGB32, width, height) as surface:
+        scale = size / coord_range.min()
+
+        with cairo.ImageSurface(cairo.FORMAT_ARGB32, size, size) as surface:
             ctx = cairo.Context(surface)
             ctx.scale(1, 1)
-            ctx.rectangle(0, 0, width, height)
+
+            # white background
+            ctx.rectangle(0, 0, size, size)
             ctx.set_source_rgb(1, 1, 1)
             ctx.fill()
+
             ctx.set_source_rgb(0, 0, 0)
-            ctx.set_line_width(1)
-            for way in self.normalized_data:
-                int_way = np.rint(way).astype(int)
-                x, y = int_way[0, :]
-                ctx.move_to(x, height - y)
-                for x, y in int_way[1:]:
-                    ctx.line_to(x, height - y)
-            ctx.stroke()
+            ctx.set_line_cap(cairo.LINE_CAP_ROUND)
+            for way, road_type in zip(self.cathographic_data, self.road_data):
+                ctx.set_line_width(line_widths.get(road_type, 1))
+                way_zeroed = np.rint((way - coord_min) * scale).astype(int)
+                x, y = way_zeroed[0, :]
+                ctx.move_to(x, size - y)
+                for x, y in way_zeroed[1:]:
+                    ctx.line_to(x, size - y)
+                ctx.stroke()
+
+            # padding
+            ctx.set_source_rgb(1, 1, 1)
+            padding_rects = [
+                (0, 0, size, padding),
+                (0, 0, padding, size),
+                (size - padding, 0, padding, size),
+                (0, size - padding, size, padding),
+            ]
+            for rect in padding_rects:
+                ctx.rectangle(*rect)
+                ctx.fill()
 
             surface.write_to_png(filename)
 
 
-if __name__ == '__main__':
-    m = Beautymap.centered(center_latlon=(57.538498, 25.412396), size=1)
-    m.render_png("test.png")
+line_widths = dict(
+    trunk=5,
+    primary=4,
+    secondary=3,
+    tertiary=2,
+)
+m = Beautymap.centered(center_latlon=(57.538498, 25.412396), size=1.2)
+m.render_square_png("test.png", size=2000, padding=50, line_widths=line_widths)

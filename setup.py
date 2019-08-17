4f487c67fdec848539a4d8ec2d1724015f8a2949
Patrick Faion <p.faion@web.de>
2019-08-17 10:23:23 +0200

    Integrate travis and pytest

--- setup.py
+++ setup.py
@@ -1,7 +1,25 @@
 from setuptools import setup, find_packages
 
+install_requires = (
+    'pycairo',
+    'numpy',
+    'overpy',
+    'pyproj',
+)
+
+setup_requires = [
+    'pytest-runner',
+]
+
+tests_require = (
+    'pytest',
+)
+
 setup(
     name="pybeautymaps",
     version="0",
     packages=find_packages(),
+    install_requires=install_requires,
+    setup_requires=setup_requires,
+    tests_require=tests_require,
 )

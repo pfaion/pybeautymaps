07c18f161f051e3890e05672bec055878dd80397
Patrick Faion <p.faion@web.de>
2019-08-17 10:01:02 +0200

    Rework project to support testing and installing

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pybeautymaps",
+    version="0",
+    packages=find_packages(),
+)
---
+++ tests/test_beautymap.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+import pybeautymaps as pbm
+
+def test_beautymap_general_workflow(tmp_path):
+    file_path: Path = tmp_path / 'test.png'
+
+    assert not file_path.exists()
+
+    line_widths = dict(
+        trunk=5,
+        primary=4,
+        secondary=3,
+        tertiary=2,
+    )
+    m = pbm.Beautymap.centered(center_latlon=(37.030347, -93.473126), size=1.2)
+    m.render_square_png(file_path, size=1000, padding=50, line_widths=line_widths)
+
+    assert file_path.exists()
